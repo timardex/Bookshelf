eb77039d4849d241bf640070e5162cf7d7152b9a
timardex <timardex@gmail.com>
2020-05-16 13:24:27 +0200

    adding scrolltop btn

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {createRef, useState, useEffect, useCallback} from 'react';
 import BookFilter from '../Book/BookFilter';
 import BookSorter from '../Book/BookSorter';
 import BookSearch from '../Book/BookSearch';
@@ -17,17 +17,43 @@ interface Props {
 
 const Header: React.FC<Props> = (props: Props) => {
   const {booksLength, selectedFilter, setSelectedFilter, search, setSearch, bookshelf} = props;
+  const [toggle, setToggle] = useState<boolean>(false)
+
+  const headerRef = createRef<HTMLDivElement>();
+
+  const scrollToTop = (ref: any):void => {
+    ref.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  const handleScroll = useCallback((): void => {
+    setToggle(window.scrollY >= 250)
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return() => window.removeEventListener('scroll', handleScroll)
+  }, [handleScroll])
+
   return (
-    <header>
+    <header ref={headerRef}>
       <div className="top-bar">
-        <h1>Bookshelf <small>loaded books: {booksLength}</small></h1>
-        {
-          (selectedFilter !== '' || search !== '') && 
-          <ClearFilter
-            btnText={'Clear filter'}
-            setSelectedFilter={setSelectedFilter}
-            setSearch={setSearch} />
-        }
+        <div className="col-left">
+          <h1>Bookshelf <small>loaded books: {booksLength}</small></h1>
+        </div>
+
+        <div className="col-right">
+          {toggle && <div className="scroll-to-top" onClick={() => scrollToTop(headerRef)}></div>}
+          {
+            (selectedFilter !== '' || search !== '') && 
+            <ClearFilter
+              btnText={'Clear filter'}
+              setSelectedFilter={setSelectedFilter}
+              setSearch={setSearch} />
+          }
+        </div>
 
       </div>
       <BookFilter bookshelf={bookshelf} setSelectedFilter={setSelectedFilter} selectedFilter={selectedFilter}/>
