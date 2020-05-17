0e51d18c96e5c8288bc0acfdfff006d7112c8441
timardex <timardex@gmail.com>
2020-05-17 21:45:39 +0200

    refactor server

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,6 +1,6 @@
 const dataRoutes = (app, fs) => {
 
-  const dataPath = './data/data.json';
+  const dataPath = './data.json';
 
   app.get('/data', (req, res) => {
     fs.readFile(dataPath, 'utf8', (err, data) => {
