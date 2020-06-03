e7d151d56c7adf87a5e59b8502d4e7de1e81ebbe
timardex <timardex@gmail.com>
2020-06-03 13:12:08 +0200

    gh-pages

---
+++ precache-manifest.c61e40786cf7d32c25915869fe4f3068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec60282840dbb9670e04e3e0cb806e8",
+    "url": "/Bookshelf/index.html"
+  },
+  {
+    "revision": "f0476729648dbd87f335",
+    "url": "/Bookshelf/static/css/main.5fc3b486.chunk.css"
+  },
+  {
+    "revision": "c3978adea50966ee62ff",
+    "url": "/Bookshelf/static/js/2.f37e7025.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bookshelf/static/js/2.f37e7025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0476729648dbd87f335",
+    "url": "/Bookshelf/static/js/main.df43fdd7.chunk.js"
+  },
+  {
+    "revision": "edf2695459bf4998e539",
+    "url": "/Bookshelf/static/js/runtime-main.6cca6cfc.js"
+  }
+]);
